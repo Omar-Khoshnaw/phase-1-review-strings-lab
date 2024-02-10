f247d13dc50cb3080fbb760f51aaba331019de77
Omar Sardar <omar.khoshnaw@gmail.com>
2024-02-10 20:08:28 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+let currentUser = 'Re:coded student'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
+
+console.log(shortGreeting)
